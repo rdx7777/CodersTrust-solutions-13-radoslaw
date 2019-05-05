11be7f91744a265a826c9edc3df74c130d696685
rdx7777 <radek.jerzynski@gmail.com>
2019-05-05 21:29:14 +0100

    FooBar - second commit with amendments - extracted loop and others

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -2,19 +2,23 @@ package pl.coderstrust.foobar;
 
 public class FooBar {
 
-    public static void main(String[] args) {
+    public static void printFooBar(int number) {
 
-        for (int i = 0; i <= 100; i++) {
-            System.out.print(i + " ");
-            if (i%3 == 0) {
-                System.out.print("Foo");
+        for (int i = 0; i <= number; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(i + " ");
+            if (i % 3 == 0) {
+                stringBuilder.append("Foo");
             }
-            if (i%5 == 0) {
-                System.out.print("Bar");
+            if (i % 5 == 0) {
+                stringBuilder.append("Bar");
             }
-            System.out.println();
+            System.out.println(stringBuilder);
         }
-
     }
 
+    public static void main(String[] args) {
+
+        printFooBar(100);
+    }
 }
