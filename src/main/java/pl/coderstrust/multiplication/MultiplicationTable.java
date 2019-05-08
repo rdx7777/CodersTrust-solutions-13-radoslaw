81e8b0047e12366cb5fa1a56fb4ccf50da5489af
rdx7777 <radek.jerzynski@gmail.com>
2019-05-08 19:09:59 +0100

    MultiplicationTable - second commit - code completely changed

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -2,66 +2,51 @@ package pl.coderstrust.multiplication;
 
 public class MultiplicationTable {
 
-    public static void main(String[] args) {
+    private static int spaceRepeater;
 
+    public static void main(String[] args) {
         printMultiplicationTable(3);
         System.out.println();
-        printMultiplicationTable(9);
-        System.out.println();
         printMultiplicationTable(12);
+        System.out.println();
+        printMultiplicationTable(32);
     }
 
     public static void printMultiplicationTable(int size) {
-
-        String space = " ";
-        int product;
-        int sizeSpacing = Integer.toString(size*size).length();
-        StringBuilder stringBuilder = new StringBuilder();
-        if (size < 1 || size > 12) {
-            System.out.println("Sorry!");
-        } else {
-            // first row
-            stringBuilder.append(beginningOfTheLine(1, space));
-            for (int i = 1; i < size + 1; i++) {
-                stringBuilder.append(spaceBetweenProducts(i, space, sizeSpacing)).append(i);
+        // header
+        for (int i = 1; i <= size; i++) {
+            printFirstRow(i, size);
+        }
+        System.out.print("\n".repeat(2));
+        // table
+        for (int i = 1; i <= size; i++) {
+            printNumberInFirstColumn(i, size);
+            for (int j = 1; j <= size; j++) {
+                printResultOfMultiplication(i, j, size);
             }
-            System.out.println(stringBuilder);
-            // multiplication table
-            for (int i = 1; i <= size; i++) {
-                stringBuilder.setLength(0);
-                stringBuilder.append(beginningOfTheLine(i + 1, space));
-                for (int j = 1; j <= size; j++) {
-                    product = (i) * j;
-                    stringBuilder.append(spaceBetweenProducts(product, space, sizeSpacing)).append(product);
-                }
-                System.out.println(stringBuilder);
-            }
+            System.out.print("\n".repeat(2));
         }
     }
 
-    public static String beginningOfTheLine(int number, String space) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-        int counter = 3 - Integer.toString(number - 1).length();
-        for (int i = counter; i > 0; i--) {
-            stringBuilder.append(space);
+    public static void printFirstRow(int i, int size) {
+        spaceRepeater = Integer.toString(size * size).length() + 2 - Integer.toString(i).length();
+        switch (i) {
+            case 1:
+                System.out.print(String.format(" ".repeat(Integer.toString(size).length() + 1)));
+                System.out.print(String.format("%s%d", " ".repeat(spaceRepeater), i));
+                break;
+            default:
+                System.out.print(String.format("%s%d", " ".repeat(spaceRepeater), i));
         }
-        if (number == 1) {
-            stringBuilder.append(" ");
-        } else {
-            stringBuilder.append(number - 1);
-
-        }
-        return stringBuilder.toString();
     }
 
-    public static String spaceBetweenProducts(int product, String space, int sizeSpacing) {
+    public static void printNumberInFirstColumn(int i, int size) {
+        spaceRepeater = Integer.toString(size).length() + 1 - Integer.toString(i).length();
+        System.out.print(String.format("%s%d", " ".repeat(spaceRepeater), i));
+    }
 
-        int counter = sizeSpacing + 2 - Integer.toString(product).length();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = counter; i > 0; i--) {
-            stringBuilder.append(space);
-        }
-        return stringBuilder.toString();
+    public static void printResultOfMultiplication(int i, int j, int size) {
+        spaceRepeater = Integer.toString(size * size).length() + 2 - Integer.toString(i * j).length();
+        System.out.print(String.format("%s%d", " ".repeat(spaceRepeater), i * j));
     }
 }
