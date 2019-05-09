5e236a6b4c919e5177a4faadaf42aa9da2361803
rdx7777 <radek.jerzynski@gmail.com>
2019-05-09 21:15:12 +0100

    Pascal Triangle - second commit with amendments

--- src/main/java/pl/coderstrust/pascal/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascal/PascalTriangle.java
@@ -8,12 +8,10 @@ public class PascalTriangle {
     private static void printPascalTriangle(int number) {
         long term;
         for (int n = 0; n < number; n++) {
-            // distance of 3 spaces is adjusted for 6 characters width space for every number
             for (int i = n; i <= number; i++)
                 System.out.print(" ".repeat(3));
-            // generate current row
-            for(int k = 0; k <= n; k++) {
-                term = factorial(n) / (factorial(k) * factorial(n-k));
+            for (int k = 0; k <= n; k++) {
+                term = factorial(n) / (factorial(k) * factorial(n - k));
                 System.out.print(String.format("%6d", term));
             }
             System.out.println();
@@ -23,8 +21,7 @@ public class PascalTriangle {
     public static long factorial(int number) {
         long factorial = 1L;
         while (number >= 1) {
-            factorial *= number;
-            number--;
+            factorial *= number--;
         }
         return factorial;
     }
