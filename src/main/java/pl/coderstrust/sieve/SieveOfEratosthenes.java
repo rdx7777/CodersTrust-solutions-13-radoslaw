e84642c27644e61e877e9b0fc4593c1788ed25b6
rdx7777 <radek.jerzynski@gmail.com>
2019-05-13 10:15:58 +0100

    Sieve - second commit with amendments

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -2,22 +2,32 @@ package pl.coderstrust.sieve;
 
 public class SieveOfEratosthenes {
     public static int[] sieve(int maximumNumber) {
+        if (maximumNumber < 2) {
+            return new int[0];
+        }
+        int[] array = createInitialArray(maximumNumber);
+        markNonePrimeNumbers(array);
+        return extractPrimesNumbers(array);
+    }
 
+    private static int[] createInitialArray(int maximumNumber) {
         int[] array = new int[maximumNumber];
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
         }
+        return array;
+    }
+
+    private static void markNonePrimeNumbers(int[] array) {
         for (int i = 2; i < array.length; i++) {
             for (int j = i * 2; j < array.length; j += i) {
                 array[j] = 0;
             }
         }
-        int numberOfPrimes = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > 1) {
-                numberOfPrimes++;
-            }
-        }
+    }
+
+    private static int[] extractPrimesNumbers(int[] array) {
+        int numberOfPrimes = getCountOfPrimeNumbers(array);
         int[] arrayOfPrimes = new int[numberOfPrimes];
         int indexOfArrayOfPrimes = 0;
         for (int i = 0; i < array.length; i++) {
@@ -26,7 +36,16 @@ public class SieveOfEratosthenes {
                 indexOfArrayOfPrimes++;
             }
         }
-        System.out.print(indexOfArrayOfPrimes + " / " + maximumNumber + " ---> ");
         return arrayOfPrimes;
+    }
+
+    private static int getCountOfPrimeNumbers(int[] array) {
+        int numberOfPrimes = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 1) {
+                numberOfPrimes++;
+            }
+        }
+        return numberOfPrimes;
     }
 }
--- src/test/java/pl/coderstrust/sieve/SieveOfEratosthenesTest.java
+++ src/test/java/pl/coderstrust/sieve/SieveOfEratosthenesTest.java
@@ -1,49 +1,10 @@
 package pl.coderstrust.sieve;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SieveOfEratosthenesTest {
-
-    @Test
-    public void shouldReturnArrayOfPrimeNumberTill0() {
-        // given
-        int maximumNumber = 0;
-        int[] expected = {};
-
-        // when
-        int[] actual = SieveOfEratosthenes.sieve(maximumNumber);
-
-        // then
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void shouldReturnArrayOfPrimeNumberTill1() {
-        // given
-        int maximumNumber = 1;
-        int[] expected = {};
-
-        // when
-        int[] actual = SieveOfEratosthenes.sieve(maximumNumber);
-
-        // then
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void shouldReturnArrayOfPrimeNumberTill30() {
-        // given
-        int maximumNumber = 30;
-        int[] expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-
-        // when
-        int[] actual = SieveOfEratosthenes.sieve(maximumNumber);
-
-        // then
-        assertThat(actual).isEqualTo(expected);
-    }
 
     @Test
     public void shouldReturnArrayOfPrimeNumberTill100() {
