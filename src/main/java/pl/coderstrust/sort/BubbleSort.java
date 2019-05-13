4c681698407a1c0cfdc084ac2b48f20786d92085
rdx7777 <radek.jerzynski@gmail.com>
2019-05-13 09:28:10 +0100

    Sorting - third commit - with amendments (partly not working)

--- src/main/java/pl/coderstrust/sort/BubbleSort.java
+++ src/main/java/pl/coderstrust/sort/BubbleSort.java
@@ -3,21 +3,20 @@ package pl.coderstrust.sort;
 public class BubbleSort {
 
     public static int[] sort(int[] array) {
-
-        int[] sortedArray = new int[array.length];
-        for (int i = 0; i < sortedArray.length; i++) {
-            sortedArray[i] = array[i];
-        }
+        int[] sortedArray = array.clone();
         for (int i = 0; i < sortedArray.length; i++) {
             for (int j = 1; j < sortedArray.length; j++) {
-                int a = sortedArray[j - 1];
-                int b = sortedArray[j];
-                if (a > b) {
-                    sortedArray[j - 1] = b;
-                    sortedArray[j] = a;
+                if (sortedArray[j - 1] > sortedArray[j]) {
+                    swapElementsInArray(sortedArray, j - 1, j);
                 }
             }
         }
         return sortedArray;
+    }
+
+    private static void swapElementsInArray(int[] array, int indexOfFirstElement, int indexOfSecondElement) {
+        int temp = array[indexOfSecondElement];
+        array[indexOfSecondElement] = array[indexOfFirstElement];
+        array[indexOfFirstElement] = temp;
     }
 }
--- src/main/java/pl/coderstrust/sort/SelectionSort.java
+++ src/main/java/pl/coderstrust/sort/SelectionSort.java
@@ -3,11 +3,7 @@ package pl.coderstrust.sort;
 public class SelectionSort {
 
     public static int[] sort(int[] array) {
-
-        int[] sortedArray = new int[array.length];
-        for (int i = 0; i < sortedArray.length; i++) {
-            sortedArray[i] = array[i];
-        }
+        int[] sortedArray = array.clone();
         for (int i = 0; i < sortedArray.length; i++) {
             int checked = sortedArray[i];
             for (int j = i + 1; j < sortedArray.length; j++) {
@@ -19,5 +15,27 @@ public class SelectionSort {
             }
         }
         return sortedArray;
+    }
+
+    // version with amendments does'n work properly
+
+//    public static int[] sort(int[] array) {
+//        int[] sortedArray = array.clone();
+//        for (int i = 0; i < sortedArray.length; i++) {
+//            int minimalElementPosition = i;
+//            for (int j = i + 1; j < sortedArray.length; j++) {
+//                if (sortedArray[j] < sortedArray[minimalElementPosition]) {
+//                    minimalElementPosition = j;
+//                    swapElementsInArray(sortedArray, minimalElementPosition, i);
+//                }
+//            }
+//        }
+//        return sortedArray;
+//    }
+
+    private static void swapElementsInArray(int[] array, int indexOfFirstElement, int indexOfSecondElement) {
+        int temp = array[indexOfSecondElement];
+        array[indexOfSecondElement] = array[indexOfFirstElement];
+        array[indexOfFirstElement] = temp;
     }
 }
