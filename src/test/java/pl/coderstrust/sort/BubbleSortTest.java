3d1c6bb744f7567a056b27265e2fe9dfcc689e19
rdx7777 <radek.jerzynski@gmail.com>
2019-05-15 23:53:36 +0100

    Sorting - eighth commit - with corrected providing test data methods

--- src/test/java/pl/coderstrust/sort/BubbleSortTest.java
+++ src/test/java/pl/coderstrust/sort/BubbleSortTest.java
@@ -13,30 +13,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BubbleSortTest {
 
     @ParameterizedTest
-    @MethodSource("dataForTests")
+    @MethodSource("shouldSortArguments")
     void shouldSort(int[] input, int[] expected) {
         assertThat(BubbleSort.sort(input)).isEqualTo(expected);
     }
 
-    static Stream<Arguments> dataForTests() {
-        int[] inputNull = {};
-        int[] expectedNull = {};
-        int[] inputOneElement = {1};
-        int[] expectedOneElement = {1};
-        int[] inputSameElements = {5, 5, 5, 5, 5, 5, 5};
-        int[] expectedSameElements = {5, 5, 5, 5, 5, 5, 5};
-        int[] inputAlreadySortedElements = {1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99};
-        int[] expectedAlreadySortedElements = {1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99};
-        int[] inputShuffledElements = {3, 99, 2, 56, 43, 15, -2, 11, 10, 1, 12, 8};
-        int[] expectedShuffledElements = {-2, 1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99};
-        int[] inputReverseSortedElements = {99, 56, 43, 15, 12, 11, 10, 8, 3, 2, 1};
-        int[] expectedReverseSortedElements = {1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99};
-        int[] inputContainingDuplicatedElements = {3, 0, -2, 99, 2, 1, 3, 56, 43, 15, 12, 11, 10, -11, 43, 1, 12, 8, -3, -2};
-        int[] expectedContainingDuplicatedElements = {-11, -3, -2, -2, 0, 1, 1, 2, 3, 3, 8, 10, 11, 12, 12, 15, 43, 43, 56, 99};
-        return Stream.of(Arguments.of(inputNull, expectedNull), Arguments.of(inputOneElement, expectedOneElement),
-                Arguments.of(inputSameElements, expectedSameElements), Arguments.of(inputAlreadySortedElements, expectedAlreadySortedElements),
-                Arguments.of(inputShuffledElements, expectedShuffledElements), Arguments.of(inputReverseSortedElements, expectedReverseSortedElements),
-                Arguments.of(inputContainingDuplicatedElements, expectedContainingDuplicatedElements));
+    static Stream<Arguments> shouldSortArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{}, new int[]{}),
+                Arguments.of(new int[]{1}, new int[]{1}),
+                Arguments.of(new int[]{5, 5, 5, 5, 5, 5, 5}, new int[]{5, 5, 5, 5, 5, 5, 5}),
+                Arguments.of(new int[]{1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99}, new int[]{1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99}),
+                Arguments.of(new int[]{3, 99, 2, 56, 43, 15, -2, 11, 10, 1, 12, 8}, new int[]{-2, 1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99}),
+                Arguments.of(new int[]{99, 56, 43, 15, 12, 11, 10, 8, 3, 2, 1}, new int[]{1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99}),
+                Arguments.of(new int[]{1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99}, new int[]{1, 2, 3, 8, 10, 11, 12, 15, 43, 56, 99}),
+                Arguments.of(new int[]{3, 0, -2, 99, 2, 1, 3, 56, 43, 15, 12, 11, 10, -11, 43, 1, 12, 8, -3, -2}, new int[]{-11, -3, -2, -2, 0, 1, 1, 2, 3, 3, 8, 10, 11, 12, 12, 15, 43, 43, 56, 99})
+        );
     }
 
     @Test
