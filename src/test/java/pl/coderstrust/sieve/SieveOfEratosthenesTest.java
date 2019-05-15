cbf1b9050cb95d833dbc67920fc371c3f4945622
rdx7777 <radek.jerzynski@gmail.com>
2019-05-16 00:16:18 +0100

    Sieve - fourth commit - with corrected providing test data methods

--- src/test/java/pl/coderstrust/sieve/SieveOfEratosthenesTest.java
+++ src/test/java/pl/coderstrust/sieve/SieveOfEratosthenesTest.java
@@ -11,20 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SieveOfEratosthenesTest {
 
     @ParameterizedTest
-    @MethodSource("dataForTests")
+    @MethodSource("sieveArguments")
     void shouldSieve(int maximumNumber, int[] expected) {
         assertThat(SieveOfEratosthenes.sieve(maximumNumber)).isEqualTo(expected);
     }
 
-    static Stream<Arguments> dataForTests() {
-        int numberLowerThanZero = -5;
-        int[] arrayExpectedForNumberLowerThanZero = new int[0];
-        int numberLowerThanTwo = 1;
-        int[] arrayExpectedForNumberLowerThanTwo = new int[0];
-        int numberGreaterThanTwo = 100;
-        int[] arrayExpectedForNumberGreaterThanTwo = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-        return Stream.of(Arguments.of(numberLowerThanZero, arrayExpectedForNumberLowerThanZero),
-                Arguments.of(numberLowerThanTwo, arrayExpectedForNumberLowerThanTwo),
-                Arguments.of(numberGreaterThanTwo, arrayExpectedForNumberGreaterThanTwo));
+    static Stream<Arguments> sieveArguments() {
+        return Stream.of(
+                Arguments.of(-5, new int[0]),
+                Arguments.of(1, new int[0]),
+                Arguments.of(100, new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97})
+        );
     }
 }
