50f88c3e3f460089a53e858c94020c369ecc5146
rdx7777 <radek.jerzynski@gmail.com>
2019-05-18 00:12:30 +0100

    FooBar - first tests commit

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -1,23 +1,31 @@
 package pl.coderstrust.foobar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FooBar {
 
-    public static void main(String[] args) {
-        printFooBar(100);
-    }
-
-    public static void printFooBar(int number) {
+    public static List<String> getFooBar(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Passed number cannot be lower than zero.");
+        }
+        List<String> result = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i <= number; i++) {
             stringBuilder.setLength(0);
-            stringBuilder.append(i).append(" ");
+            stringBuilder.append(i);
             if (i % 3 == 0) {
-                stringBuilder.append("Foo");
+                stringBuilder.append(" ").append("Foo");
             }
             if (i % 5 == 0) {
-                stringBuilder.append("Bar");
+                if (stringBuilder.toString().contains("o")) {
+                    stringBuilder.append("Bar");
+                } else {
+                    stringBuilder.append(" ").append("Bar");
+                }
             }
-            System.out.println(stringBuilder);
+            result.add(stringBuilder.toString());
         }
+        return result;
     }
 }
---
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -0,0 +1,38 @@
+package pl.coderstrust.foobar;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FooBarTest {
+
+    @ParameterizedTest
+    @MethodSource("fooBarArguments")
+    void shouldReturnCorrectFooBar(int number, List<String> expected) {
+        assertThat(FooBar.getFooBar(number)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> fooBarArguments() {
+        List<String> expectedFor3 = Arrays.asList("0 FooBar", "1", "2", "3 Foo");
+        List<String> expectedFor16 = Arrays.asList("0 FooBar", "1", "2", "3 Foo", "4", "5 Bar", "6 Foo", "7", "8",
+                "9 Foo", "10 Bar", "11", "12 Foo", "13", "14", "15 FooBar", "16");
+        return Stream.of(
+                Arguments.of(3, expectedFor3),
+                Arguments.of(16, expectedFor16)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-2, -5, -33})
+    void shouldThrowExceptionForInvalidArgument(int size) {
+        assertThrows(IllegalArgumentException.class, () -> FooBar.getFooBar(size));
+    }
+}
