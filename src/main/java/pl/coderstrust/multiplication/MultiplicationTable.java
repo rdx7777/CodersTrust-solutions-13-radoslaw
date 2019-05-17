f0095f0d1842bc9b232de860cb6ab3c1c5b003dc
rdx7777 <radek.jerzynski@gmail.com>
2019-05-17 11:20:55 +0100

    MultiplicationTable - first tests commit

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -1,30 +1,56 @@
 package pl.coderstrust.multiplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        printMultiplicationTable(3);
-        System.out.println();
-        printMultiplicationTable(12);
-        System.out.println();
-        printMultiplicationTable(32);
-    }
-
-    public static void printMultiplicationTable(int size) {
-        String singleNumberPattern = getFormattingPatternForSingleNumber(size);
-        String multiplicationPattern = getFormattingPatternForMultiplication(size);
-        System.out.printf(singleNumberPattern, "");
-        for (int i = 1; i <= size; i++) {
-            System.out.printf(multiplicationPattern, i);
+        List<String> multiplicationTable = printMultiplicationTable(3);
+        for (int i = 0; i < multiplicationTable.size(); i++) {
+            System.out.println(multiplicationTable.get(i));
         }
+//        List<String> multiplicationTable2 = printMultiplicationTable(12);
+//        for (int i = 0; i < multiplicationTable2.size(); i++) {
+//            System.out.println(multiplicationTable2.get(i));
+//        }
+//        List<String> multiplicationTable3 = printMultiplicationTable(32);
+//        for (int i = 0; i < multiplicationTable3.size(); i++) {
+//            System.out.println(multiplicationTable3.get(i));
+//        }
         System.out.println();
-        for (int i = 1; i <= size; i++) {
-            System.out.printf(singleNumberPattern, i);
-            for (int j = 1; j <= size; j++) {
-                System.out.printf(multiplicationPattern, i * j);
+        int size = 6;
+        int[][] multiplicationArray = printMultiplicationTableUsingTwoDimensionalArray(size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(multiplicationArray[i][j] + "  ");
             }
             System.out.println();
         }
+    }
+
+    public static List<String> printMultiplicationTable(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Multiplication table can be printed only for numbers greater than zero.");
+        }
+        List<String> result = new ArrayList<>();
+        StringBuilder multiplicationTable = new StringBuilder();
+        String singleNumberPattern = getFormattingPatternForSingleNumber(size);
+        String multiplicationPattern = getFormattingPatternForMultiplication(size);
+        multiplicationTable.append(String.format(singleNumberPattern, ""));
+        for (int i = 1; i <= size; i++) {
+            multiplicationTable.append(String.format(multiplicationPattern, i));
+        }
+        result.add(multiplicationTable.toString());
+        for (int i = 1; i <= size; i++) {
+            multiplicationTable.setLength(0);
+            multiplicationTable.append(String.format(singleNumberPattern, i));
+            for (int j = 1; j <= size; j++) {
+                multiplicationTable.append(String.format(multiplicationPattern, i * j));
+            }
+            result.add(multiplicationTable.toString());
+        }
+        return result;
     }
 
     private static String getFormattingPatternForSingleNumber(int number) {
@@ -35,5 +61,18 @@ public class MultiplicationTable {
     private static String getFormattingPatternForMultiplication(int number) {
         int width = String.valueOf(number * number).length() + 1;
         return "%" + width + "s";
+    }
+
+    public static int[][] printMultiplicationTableUsingTwoDimensionalArray(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Multiplication table can be printed only for numbers greater than zero.");
+        }
+        int[][] resultArray = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                resultArray[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return resultArray;
     }
 }
---
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -0,0 +1,75 @@
+package pl.coderstrust.multiplication;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MultiplicationTableTest {
+
+    @ParameterizedTest
+    @MethodSource("multiplicationTableArguments")
+    void testPrintMultiplicationTable(int size, List<String> expected) {
+        assertThat(MultiplicationTable.printMultiplicationTable(size)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> multiplicationTableArguments() {
+        List<String> expectedListFor3 = new ArrayList<>();
+        expectedListFor3.add("  1 2 3");
+        expectedListFor3.add("1 1 2 3");
+        expectedListFor3.add("2 2 4 6");
+        expectedListFor3.add("3 3 6 9");
+        List<String> expectedListFor12 = new ArrayList<>();
+        expectedListFor12.add("     1   2   3   4   5   6   7   8   9  10  11  12");
+        expectedListFor12.add(" 1   1   2   3   4   5   6   7   8   9  10  11  12");
+        expectedListFor12.add(" 2   2   4   6   8  10  12  14  16  18  20  22  24");
+        expectedListFor12.add(" 3   3   6   9  12  15  18  21  24  27  30  33  36");
+        expectedListFor12.add(" 4   4   8  12  16  20  24  28  32  36  40  44  48");
+        expectedListFor12.add(" 5   5  10  15  20  25  30  35  40  45  50  55  60");
+        expectedListFor12.add(" 6   6  12  18  24  30  36  42  48  54  60  66  72");
+        expectedListFor12.add(" 7   7  14  21  28  35  42  49  56  63  70  77  84");
+        expectedListFor12.add(" 8   8  16  24  32  40  48  56  64  72  80  88  96");
+        expectedListFor12.add(" 9   9  18  27  36  45  54  63  72  81  90  99 108");
+        expectedListFor12.add("10  10  20  30  40  50  60  70  80  90 100 110 120");
+        expectedListFor12.add("11  11  22  33  44  55  66  77  88  99 110 121 132");
+        expectedListFor12.add("12  12  24  36  48  60  72  84  96 108 120 132 144");
+        return Stream.of(
+                Arguments.of(3, expectedListFor3),
+                Arguments.of(12, expectedListFor12)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -3, -15})
+    void shouldThrowExceptionForInvalidArgument(int size) {
+        assertThrows(IllegalArgumentException.class, () -> MultiplicationTable.printMultiplicationTable(size));
+    }
+
+    @ParameterizedTest
+    @MethodSource("multiplicationTableUsingTwoDimensionalArrayArguments")
+    void testPrintMultiplicationTableUsingTwoDimensionalArray(int size, int[][] expected) {
+        assertThat(MultiplicationTable.printMultiplicationTableUsingTwoDimensionalArray(size)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> multiplicationTableUsingTwoDimensionalArrayArguments() {
+        return Stream.of(
+                Arguments.of(3, new int[][]{{1, 2, 3}, {2, 4, 6}, {3, 6, 9}}),
+                Arguments.of(7, new int[][]{{1, 2, 3, 4, 5, 6, 7}, {2, 4, 6, 8, 10, 12, 14}, {3, 6, 9, 12, 15, 18, 21},
+                        {4, 8, 12, 16, 20, 24, 28}, {5, 10, 15, 20, 25, 30, 35}, {6, 12, 18, 24, 30, 36, 42},
+                        {7, 14, 21, 28, 35, 42, 49}})
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -3, -15})
+    void shouldThrowExceptionForInvalidArgumentInMethodUsingTwoDimensionalArray(int size) {
+        assertThrows(IllegalArgumentException.class, () -> MultiplicationTable.printMultiplicationTableUsingTwoDimensionalArray(size));
+    }
+}
