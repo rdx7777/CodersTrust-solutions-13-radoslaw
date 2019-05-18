105ada953168dd5484e7a34c67afbbf060201988
rdx7777 <radek.jerzynski@gmail.com>
2019-05-18 18:57:09 +0100

    Pascal Triangle - second tests commit - with corrected assertion and fixed arguments formatting

--- src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
+++ src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PascalTriangleTest {
 
@@ -21,11 +21,19 @@ class PascalTriangleTest {
     }
 
     static Stream<Arguments> pascalTriangleArguments() {
-        List<String> expectedFor4 = Arrays.asList("                    1", "                 1     1",
-                "              1     2     1", "           1     3     3     1");
-        List<String> expectedFor7 = Arrays.asList("                             1", "                          1     1",
-                "                       1     2     1", "                    1     3     3     1", "                 1     4     6     4     1",
-                "              1     5    10    10     5     1", "           1     6    15    20    15     6     1");
+        List<String> expectedFor4 = Arrays.asList(
+                "                    1",
+                "                 1     1",
+                "              1     2     1",
+                "           1     3     3     1");
+        List<String> expectedFor7 = Arrays.asList(
+                "                             1",
+                "                          1     1",
+                "                       1     2     1",
+                "                    1     3     3     1",
+                "                 1     4     6     4     1",
+                "              1     5    10    10     5     1",
+                "           1     6    15    20    15     6     1");
         return Stream.of(
                 Arguments.of(4, expectedFor4),
                 Arguments.of(7, expectedFor7)
@@ -35,6 +43,6 @@ class PascalTriangleTest {
     @ParameterizedTest
     @ValueSource(ints = {-12, -7, 0})
     void shouldThrowExceptionForInvalidArgument(int number) {
-        assertThrows(IllegalArgumentException.class, () -> PascalTriangle.getPascalTriangle(number));
+        assertThatThrownBy(() -> PascalTriangle.getPascalTriangle(number)).isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
