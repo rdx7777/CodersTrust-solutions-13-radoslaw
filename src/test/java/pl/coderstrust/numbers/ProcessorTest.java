bd0e6a017a735a95cf00eee9155fe76814e7ad7d
rdx7777 <radek.jerzynski@gmail.com>
2019-05-28 10:35:25 +0100

    Numbers - second commit with new dependencies and imports in test class

--- src/test/java/pl/coderstrust/numbers/ProcessorTest.java
+++ src/test/java/pl/coderstrust/numbers/ProcessorTest.java
@@ -1,8 +1,10 @@
 package pl.coderstrust.numbers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,6 +13,7 @@ import java.util.List;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ProcessorTest {
 
     @Mock
