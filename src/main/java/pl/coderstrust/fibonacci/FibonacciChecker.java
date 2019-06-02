e69d9b756d40a74b5f5a3fdbd8467145ae197646
rdx7777 <radek.jerzynski@gmail.com>
2019-06-02 17:25:13 +0100

    IsFibonacci - third commit with corrected expressions

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -11,20 +11,20 @@ public class FibonacciChecker {
             throw new IllegalArgumentException("Fibonacci number cannot be lower than zero.");
         }
         if (number > 2000000000) {
-            throw new IllegalArgumentException("Number out of supported range.");
+            throw new IllegalArgumentException("Number out of supported range. The maximum supported number is 2000000000.");
         }
         if (cache.containsKey(number)) {
             return cache.get(number);
         }
-        long formula1 = 5 * number * number + 4;
-        long formula2 = 5 * number * number - 4;
+        long formula1 = 5 * (number * number) + 4;
+        long formula2 = 5 * (number * number) - 4;
         boolean isFibonacciNumber = isPerfectSquare(formula1) || isPerfectSquare(formula2);
         cache.put(number, isFibonacciNumber);
         return isFibonacciNumber;
     }
 
     public static boolean isPerfectSquare(long number) {
-        double squareRootOfNumber = Math.sqrt(number);
-        return squareRootOfNumber == Math.round(squareRootOfNumber);
+        double root = Math.sqrt(number);
+        return root == Math.round(root);
     }
 }
