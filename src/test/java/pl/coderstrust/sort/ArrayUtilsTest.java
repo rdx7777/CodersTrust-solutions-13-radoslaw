d2a6207d55b4f29770e056a0daaff1782ab82c39
rdx7777 <radek.jerzynski@gmail.com>
2019-06-07 23:46:04 +0100

    Let's sort & compare - fourth commit with changed ArrayUtilsTest class

--- src/test/java/pl/coderstrust/sort/ArrayUtilsTest.java
+++ src/test/java/pl/coderstrust/sort/ArrayUtilsTest.java
@@ -1,5 +1,6 @@
 package pl.coderstrust.sort;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,37 +13,50 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class ArrayUtilsTest {
 
     @ParameterizedTest
-    @MethodSource("arrayUtilsArguments")
+    @MethodSource("swapElementsArguments")
     void shouldSwapElementsInArray(int[] input, int indexOfFirstElement, int indexOfSecondElement, int[] expected) {
         ArrayUtils.swapElementsInArray(input, indexOfFirstElement, indexOfSecondElement);
         assertArrayEquals(input, expected);
     }
 
-    static Stream<Arguments> arrayUtilsArguments() {
+    static Stream<Arguments> swapElementsArguments() {
         return Stream.of(
                 Arguments.of(new int[]{1, 2, 3, 4, 5}, 2, 3, new int[]{1, 2, 4, 3, 5}),
                 Arguments.of(new int[]{1, 2, 3, 4, 5}, 0, 4, new int[]{5, 2, 3, 4, 1}),
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 2, 2, new int[]{1, 2, 3, 4, 5}),
                 Arguments.of(new int[]{1, 3, 3, 3, 3, 3, 4, 7, 6, 10}, 1, 2, new int[]{1, 3, 3, 3, 3, 3, 4, 7, 6, 10}),
                 Arguments.of(new int[]{0, 0, 0, 0, 0, 0}, 0, 5, new int[]{0, 0, 0, 0, 0, 0})
         );
     }
 
+    @Test
+    void shouldThrowExceptionForNullAsArray() {
+        assertThrows(IllegalArgumentException.class, () -> ArrayUtils.swapElementsInArray(null, 1, 2));
+    }
+
     @ParameterizedTest
-    @MethodSource("invalidArrayUtilsArguments")
-    void shouldThrowExceptionForInvalidArguments(int[] input, int indexOfFirstElement, int indexOfSecondElement) {
+    @MethodSource("invalidIndexOfFirstElementArguments")
+    void shouldThrowExceptionForInvalidIndexOfFirstElement(int[] input, int indexOfFirstElement, int indexOfSecondElement) {
         assertThrows(IllegalArgumentException.class, () -> ArrayUtils.swapElementsInArray(input, indexOfFirstElement, indexOfSecondElement));
     }
 
-    static Stream<Arguments> invalidArrayUtilsArguments() {
+    static Stream<Arguments> invalidIndexOfFirstElementArguments() {
         return Stream.of(
-                Arguments.of(null, 1, 2),
                 Arguments.of(new int[]{1, 2, 3, 4, 5}, -1, 3),
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, 2, 5),
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, -2, 4),
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, 1, 7),
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, -1, -1),
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, 6, 7),
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, -1, 7)
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 6, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidIndexOfSecondElementArguments")
+    void shouldThrowExceptionForInvalidIndexOfSecondElement(int[] input, int indexOfFirstElement, int indexOfSecondElement) {
+        assertThrows(IllegalArgumentException.class, () -> ArrayUtils.swapElementsInArray(input, indexOfFirstElement, indexOfSecondElement));
+    }
+
+    static Stream<Arguments> invalidIndexOfSecondElementArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 1, -1),
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 1, 7)
         );
     }
 }
