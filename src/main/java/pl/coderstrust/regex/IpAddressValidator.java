dc677b63bb9070f9bb24e82f50f47fb06a220b57
rdx7777 <radek.jerzynski@gmail.com>
2019-06-27 11:52:52 +0100

    Regex - third commit with amendments

--- src/main/java/pl/coderstrust/regex/IpAddressValidator.java
+++ src/main/java/pl/coderstrust/regex/IpAddressValidator.java
@@ -4,14 +4,12 @@ import java.util.regex.Pattern;
 
 public class IpAddressValidator {
 
-    private static final String IP_ADDRESS_PATTERN
-            = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
-
+    private static final String IP_ADDRESS_PATTERN = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
     private static Pattern pattern = Pattern.compile(IP_ADDRESS_PATTERN);
 
     public static boolean isIpAddress(String input) {
         if (input == null) {
-            throw new IllegalArgumentException("Passed string cannot be null.");
+            throw new IllegalArgumentException("Input cannot be null.");
         }
         return pattern.matcher(input).matches();
     }
