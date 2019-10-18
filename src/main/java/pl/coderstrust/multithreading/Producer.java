9eebb155562d7b29d0d1c793a0678262b9df88e1
rdx7777 <radek.jerzynski@gmail.com>
2019-10-18 09:59:43 +0100

    Multithreading - added validation and other changes to consumer and producer classes

--- src/main/java/pl/coderstrust/multithreading/Producer.java
+++ src/main/java/pl/coderstrust/multithreading/Producer.java
@@ -5,11 +5,23 @@ import java.util.concurrent.BlockingQueue;
 
 public class Producer implements Runnable {
 
-    private BlockingQueue<String> queue;
-    private Duration duration;
-    private int amount;
+    private final BlockingQueue<String> queue;
+    private final Duration duration;
+    private final int amount;
 
-    public Producer(BlockingQueue<String> queue, Duration duration, int amount) {
+    Producer(BlockingQueue<String> queue, Duration duration, int amount) {
+        if (queue == null) {
+            throw new IllegalArgumentException("Queue cannot be null.");
+        }
+        if (duration == null) {
+            throw new IllegalArgumentException("Duration cannot be null.");
+        }
+        if (duration.isNegative() || duration.isZero()) {
+            throw new IllegalArgumentException("Duration must be positive.");
+        }
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive.");
+        }
         this.queue = queue;
         this.duration = duration;
         this.amount = amount;
@@ -17,15 +29,15 @@ public class Producer implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < amount; i++) {
-            System.out.println("Produced: " + i);
-            try {
+        try {
+            for (int i = 0; i < amount; i++) {
+                System.out.println("Produced: " + i);
                 Thread.sleep(duration.toMillis());
                 queue.put("Result " + i + " by " + Thread.currentThread().getName());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
